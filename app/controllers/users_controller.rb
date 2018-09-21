4895ddc686887587cef7c7da61c03f5a43826905
takurooon <takurokodama0726@gmail.com>
2018-09-21 13:39:14 +0900

    fix views blogs index 7lines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,15 +11,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       redirect_to user_path(@user.id)
     else
       render "new"
     end
-
   end
-
+  
 
   private
 
