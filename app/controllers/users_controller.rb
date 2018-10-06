a8c3d4d031373ff0778c5ffe86ad6d6e8e50346a
takurooon <takurokodama0726@gmail.com>
2018-10-06 13:35:45 +0900

    chage users_controller create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to new_session_path(@user.id)
     else
       render "new"
     end
   end
-  
+
 
   private
 
